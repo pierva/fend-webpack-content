caabd050c479ca2b18912f9d004a6e27fda56660
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-12-01 16:44:03 -0500

    fix: Javascrpit scope with Client library

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,14 @@
 import { handleSubmit } from './js/formHandler'
-import { nameChecker } from './js/nameChecker'
+import { checkForName } from './js/nameChecker'
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/form.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
+
+export {
+    checkForName,
+    handleSubmit
+}
 
 
