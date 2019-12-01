d8a1b33c3eef7114403f8a994a6ed4eea24cbc18
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-11-30 19:38:17 -0500

    feat: Import all the stylesheets

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,9 @@
 import { handleSubmit } from './js/formHandler'
 import { nameChecker } from './js/nameChecker'
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+
+
